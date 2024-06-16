e401d0c98f369c22fe5d0711f01cb21b3f291c4e
Bryan Leonard <bryan.leonard75@gmail.com>
2024-06-16 16:02:23 -0600

    Update SelectionPane.java

--- src/scenes/title/SelectionPane.java
+++ src/scenes/title/SelectionPane.java
@@ -3,15 +3,12 @@ package scenes.title;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import utils.Globals;
-import utils.KeyHandler;
-import utils.MouseHandler;
 
 public class SelectionPane extends JPanel {
     Globals globals = new Globals();
-    MouseHandler mouseHandler = new MouseHandler();
-    KeyHandler keyHandler = new KeyHandler();
 
-    public SelectionPane() {
+    public SelectionPane()
+    {
         this.setBounds(500, 500, 500, 500);
         this.setBackground(Globals.mainBack);
         this.setLayout(null);
@@ -21,14 +18,9 @@ public class SelectionPane extends JPanel {
                 Globals.fontTitleSelect, Globals.colorTitle, 0);
         JLabel lblControls = globals.createLabel("Controls", 10, 190, 480, 50,
                 Globals.fontTitleSelect, Globals.colorTitle, 0);
-        JLabel lblExit = globals.createLabel("Press ESC or '-' to quit.", 10, 440, 480, 50,
-                Globals.fontTitleMsg, Globals.colorTitle, 0);
-        lblNewGame.addMouseListener(mouseHandler);
-        lblLoad.addMouseListener(mouseHandler);
-        lblControls.addMouseListener(mouseHandler);
+
         this.add(lblNewGame);
         this.add(lblLoad);
         this.add(lblControls);
-        this.add(lblExit);
     }
 }
