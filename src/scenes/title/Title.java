961aea218921652514ee377fbe5cf5c8aeba62ec
Bryan Leonard <bryan.leonard75@gmail.com>
2024-06-16 19:02:54 -0600

    Update Title.java

--- src/scenes/title/Title.java
+++ src/scenes/title/Title.java
@@ -4,16 +4,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import utils.Globals;
 
-import java.awt.event.MouseListener;
-
-public class Title extends JPanel {
+public class SelectionPane extends JPanel {
     Globals globals = new Globals();
 
-    public Title() {
-        this.setBounds(100, 150, 1300, 100);
+    public SelectionPane()
+    {
+        this.setBounds(500, 500, 500, 500);
         this.setBackground(Globals.mainBack);
-        JLabel lblTitle = this.globals.createLabel("Adventure", 10, 10, 1280, 80, Globals.fontTitle,
-                Globals.colorTitle, 0, false);
-        this.add(lblTitle);
+        this.setLayout(null);
+        JLabel lblNewGame = globals.createLabel("New Game", 10, 10, 480, 50,
+                Globals.fontTitleSelect, Globals.colorTitle, 0, true);
+        JLabel lblLoad = globals.createLabel("Load Game", 10, 100, 480, 50,
+                Globals.fontTitleSelect, Globals.colorTitle, 0, true);
+        JLabel lblControls = globals.createLabel("Controls", 10, 190, 480, 50,
+                Globals.fontTitleSelect, Globals.colorTitle, 0, true);
+
+        this.add(lblNewGame);
+        this.add(lblLoad);
+        this.add(lblControls);
     }
 }
